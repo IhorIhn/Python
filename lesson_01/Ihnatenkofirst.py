04c5ca17bfd9485a26fb6e6a6a36693496dacaa2
Igor Igor <igorignatenko25@gmail.com>
2025-05-25 19:57:42 +0300

    Виконані зміни

---
+++ lesson_01/Ihnatenkofirst.py
@@ -0,0 +1,68 @@
+#Task 1
+print("Hello", end=" ")
+print("world!")
+
+#Task 2
+
+hello = "Hello"
+world = "world"
+if True:
+    print(f"{hello} {world}!")
+
+#Task 3
+
+for letter in "Hello world!":
+    print(letter)
+
+#Task 4
+
+apples = 2
+banana = apples * 4
+print(banana)
+
+#Task 5
+
+storona_1 = 1
+storona_2 = 2
+сторона_3 = 3
+storona_4 = 4
+
+#Task 6
+
+storona_1 = 1
+storona_2 = 2
+сторона_3 = 3
+storona_4 = 4
+
+perimetery = storona_1 + storona_2 + сторона_3 + storona_4
+print(perimetery)
+
+#Task 7
+
+apple = 4
+pear = apple + 5
+plum = apple - 2
+sum = apple + pear + plum
+print(sum)
+
+#Task 8
+
+morning_temperature = 5.0
+afternoon_temperature = morning_temperature - 10.0
+evening_temperature = afternoon_temperature + 4.0
+print(evening_temperature)
+
+#Task 9
+
+boys = 24
+girls = 24 // 2
+
+total_kids = boys - 1 + girls -2
+print(total_kids)
+
+#Task 10
+
+first_book = 8
+second_book = first_book + 2
+third_book = (first_book + second_book) // 2
+print(first_book + second_book + third_book)
